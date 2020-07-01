a66bf67bf747756d4ed1062f6f9f3fa5c4a53b0b
Thiaguinhob13 <Thiago_melob13@hotmail.com>
2020-07-01 18:43:08 -0300

    Projeto migraçaõ

---
+++ src/main/java/com/example/thimoneyapi/ThimoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.thimoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThimoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThimoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thimoneyapi/ThimoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thimoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThimoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
